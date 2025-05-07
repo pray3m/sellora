0a309eeb2873348554b826c8706360cfa38c01ab
pray3m <prem.gtm9@gmail.com>
2025-05-07 11:38:11 +0545

    feat: implement error handling middleware and update project structure

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,33 @@
-import express from 'express';
+import { errorMiddleware } from "@sellora/error-handler/error-middleware";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.use(errorMiddleware);
+
+const server = app.listen(port, host, () => {
+  console.log(`[ AUTH SERVICE ready ] http://${host}:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.log("Server Error: ", err);
 });
